3706afa8fd2ce07d45bde52722be69a179c2ed67
Alexey198622 <46447207+Alexey198622@users.noreply.github.com>
2021-05-05 20:43:31 +0300

    Создание первой программы на Java

---
+++ src/Head_First_Java/Main.java
@@ -0,0 +1,34 @@
+package Head_First_Java;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//        int size = 27;
+//        String name = "Фидо";
+//        Dog myDog = new Dog (name, size);
+//        x = size - 5;
+//        if (x < 15) myDog.bark(8);
+//
+//        while (x > 3) {
+//            myDog.play();
+//        }
+//
+//        int[] numList = {2, 4, 6, 8};
+//        System.out.print("Привет");
+//        System.out.print("Собака: " + name);
+//        String num = "8";
+//
+//        int z = Integer.parseInt(num);
+//
+//        try {
+//            readTheFile("myFile.txt");
+//        }
+//
+//        catch (FileNotFoundException ex) {
+//            System.out.print("Файл не найден");
+//       }
+    }
+}
---
+++ src/Head_First_Java/MyFirstApp.java
@@ -0,0 +1,8 @@
+package Head_First_Java;
+
+public class MyFirstApp {
+    public static void main(String[] args) {
+        System.out.println("Java, управляет ");
+        System.out.println("миром!!!");
+    }
+}
